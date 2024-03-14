080889c60d101bfef1914bc5baa491b2eee9d68c
Sam Zack <103949002+szack25@users.noreply.github.com>
2024-03-14 19:53:54 -0400

    fetched even more shelters for shelters page by implementing a recursive function, finding next page's token

--- pages/data.py
+++ pages/data.py
@@ -1,10 +1,16 @@
 import requests
-from os import getenv
 
-
-def getLocations():
-    GOOGLE_MAPS_API_KEY = getenv('GOOGLE_MAPS_API_KEY')
-    url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.7128,-74.0060&radius=5000&keyword=homeless%20shelter&key=AIzaSyBudDAel4hrhurK0iGmhl9H3kMvzwZ3IqY'
-    response = requests.request(url=url, method="GET")
-    data = response.json()
-    return data
+def getLocations(nextPageToken = "", counter = 0, data = []):
+    pageToken = ""
+    if nextPageToken != "":
+        url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=' + nextPageToken + '&key=AIzaSyBudDAel4hrhurK0iGmhl9H3kMvzwZ3IqY'
+    else:
+        url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.7128,-74.0060&radius=5000&keyword=homeless%20shelter&key=AIzaSyBudDAel4hrhurK0iGmhl9H3kMvzwZ3IqY'
+    response = requests.request("GET", url)
+    response = response.json()
+    data.extend(response["results"])
+    if counter > 3:
+        return data
+    pageToken = response.get("next_page_token", "")
+    counter+=1
+    return getLocations(pageToken, counter, data)
