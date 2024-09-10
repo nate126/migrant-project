32342e32949d934b7e1753b4fa651e8795324301
nate126 <60267662+nate126@users.noreply.github.com>
2024-09-10 19:13:18 -0400

    Update settings.py

--- migrant_project/settings.py
+++ migrant_project/settings.py
@@ -127,11 +127,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'pages/static'
+    BASE_DIR   
+ / 'pages/static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'pages/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # New dedicated directory
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
